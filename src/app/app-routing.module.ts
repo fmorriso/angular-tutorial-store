2bfad514cd524557c96d9992fd5e897dcf73a149
Fred Morrison <fpmorrison@verizon.net>
2019-06-22 09:34:02 -0400

    add Page Not Found to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
-//
-import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, data: { label: 'Home' } },
   { path: 'about', component: AboutComponent, data: { label: 'About' } },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 const routeOptions: ExtraOptions = {
