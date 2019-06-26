64f96c0352ef0f5b735c960e38e91551b1301e18
Fred Morrison <fpmorrison@verizon.net>
2019-06-25 21:16:09 -0400

    add constructor, setters, getters to Product

--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,5 +1,24 @@
 export class Product {
-	private name: string;
-	private price: number;
-	private description: string;
+	constructor(private _name: string, private _price: number, private _description: string) {}
+	public get name(): string {
+		return this._name;
+	}
+
+	public set name(name: string) {
+		this._name = name;
+	}
+
+	public get price(): number {
+		return this._price;
+	}
+	public set price(price: number) {
+		this._price = price;
+	}
+
+	public get description(): string {
+		return this._description;
+	}
+	public set description(description: string) {
+		this._description = description;
+	}
 }
--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,4 +1,4 @@
-export const products = [
+export const products  = [
 	{
 		name: 'Phone XL',
 		price: 799,
